cd2ae8ba790f33311f1ca139ffc1fe9c234e8839
Fernando Estribi <fernandojose889@gmail.com>
2021-12-04 20:55:29 -0500

    Registrar usuario php

--- Frontend/Backendphp/registrar-usuario.php
+++ Frontend/Backendphp/registrar-usuario.php
@@ -1,11 +1,23 @@
 <?php 
- $nCedula = $_POST['nCedula'];
- $dateBirth = $_POST['dateBirth'];
+$Nombres = $_POST['Nombres'];
+$Apellidos = $_POST['Apellidos'];
+$nCedula = $_POST['nCedula'];
+$dateBirth = $_POST['dateBirth'];
+$Bloodtype = $_POST['Bloodtype'];
+$inputAddress2 = $_POST['inputAddress2'];
+
 
 $conn = new mysqli('localhost','root','','citas_medicas');
     if ($conn ->connect_error){
         die('conneción fallida: '.$conn->connect_error);
     }
     else{
-        $stmt = $conn -> prepare("");}
+        $stmt = $conn -> prepare("insert into paciente:Nombres,Apellidos,nCedula,dateBirth,Bloodtype,inputAddress2)
+            values(?,?,?,?,?,?)");
+        $stmt -> bind_param("sssiss", $Nombres,$Apellidos,$nCedula,$dateBirth,$Bloodtype,$inputAddress2);
+        $stmt -> execute(); 
+        echo "Registro exitoso";
+        $stmt -> close();
+        $conn -> close();
+    }
 ?>

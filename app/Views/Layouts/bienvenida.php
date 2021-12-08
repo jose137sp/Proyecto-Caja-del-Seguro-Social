8be2bb0c5f5a546fe8d062c6f109008b076c3604
Gerardo Fumero <gerardo.fumero@utp.ac.pa>
2021-12-08 01:41:30 -0500

    Imagen backrgound

--- app/Views/Layouts/bienvenida.php
+++ app/Views/Layouts/bienvenida.php
@@ -3,7 +3,7 @@
     <section class="py-5">
         <div class="container px-4 px-lg-5 my-5">
             <div class="row gx-4 gx-lg-5 align-items-center">
-                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="../Layouts/Images/policlinica-inicio.jpeg"
+                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://w3.css.gob.pa/wp-content/uploads/2021/10/UE-PM-PCNBSF.jpg"
                         alt="..." /></div>
                 <div class="col-md-6">
                     <h1 class="display-4 fw-bolder">Prioriza tu salud.</h1>

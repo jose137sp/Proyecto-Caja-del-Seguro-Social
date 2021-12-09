a9793cec89b4fed1fe1ef91bfec84d6a065b8129
Gerardo Fumero <gerardo.fumero@utp.ac.pa>
2021-12-08 20:18:34 -0500

    avances MVC 8:18pm

--- app/Views/Medico/medico-login.php
+++ app/Views/Medico/medico-login.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Main Stylesheet -->
-    <link rel="stylesheet" href="../Medico/css/medico-login.css">
+    <link rel="stylesheet" href="Medico/medico-login.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
---
+++ app/Views/Paciente/cita-consultada.php
@@ -0,0 +1,29 @@
+<!-- // El breadcrumb cambia en cda página. OJO CON ESTO. -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
+            <li class="breadcrumb-item">Citas programadas</li>
+            <li class="breadcrumb-item"><a href="consultar-estado.html">Consultar estado de cita</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Datos de la cita</li>
+        </ol>
+    </nav>
+
+    <!-- Contenido de la sección -->
+    <div class="container-fluid">
+        <div class="jumbotron text-center">
+            <h4 class="display-4">Estado de la cita:</h4>
+            <p class="lead"><strong>Fecha: </strong>00/00/0000</p>
+            <p class="lead"><strong>Policlínica: </strong>xxxxxxxxx</p>
+            <p class="lead"><strong>Numéro de cita: </strong>xxxxxxxxx</p>
+            <p class="lead"><strong>Estado: </strong>xxxxxxxxx</p>
+            <hr>
+            <a class="a" href="consultar-estado-cancelar.html" style="color: #eb0000; font-weight: bold;">Cancelar cita</a>
+            <hr>
+            <p>
+                ¿Tienes problemas? <a href="">Contáctanos</a>
+            </p>
+            <p class="lead">
+                <a class="btn" href="index.html" role="button" style="background-color: #005C8F; color: white;">Menú principal</a>
+            </p>
+        </div>
+    </div>

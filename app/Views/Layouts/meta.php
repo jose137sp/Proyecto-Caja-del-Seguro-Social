d8fe92366447ef859f891ed87e7211579171c155
Jose <13joselon@gmail.com>
2021-12-11 04:44:11 -0500

    se agregó favicon

--- app/Views/Layouts/meta.php
+++ app/Views/Layouts/meta.php
@@ -1,6 +1,9 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="app/Views/Layouts/favicon.ico" />
+
     <!-- Main Stylesheet -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">

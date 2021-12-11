b30cf57852dd85eddd00fceaa1505d600994cc41
Gerardo Fumero <gerardo.fumero@utp.ac.pa>
2021-12-11 09:14:05 -0500

    Entrega Final

--- app/Db/db.php
+++ app/Db/db.php
@@ -3,7 +3,7 @@ class Conexion
 {
         public static function conectar()
         {
-            $conexion = new mysqli('sql103.epizy.com', 'epiz_30546817', 'rj62aAYbhO', 'epiz_30546817_gestion_citas');
+            $conexion = new mysqli('localhost', 'root', '', 'gestion_cita');
             return $conexion;
         }
 }
